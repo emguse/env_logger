65e57c5d10f880be6bbcbfa79b61ad8daa8cb8b6
emguse <30695541+emguse@users.noreply.github.com>
2021-10-28 21:53:58 +0900

    fix datetime -> str

--- env_logger.py
+++ env_logger.py
@@ -59,7 +59,7 @@ class thp_logger():
         press = round(press, 4)
         data = [temperature, humidity, press]
         print(data)
-        msg = {'time':datetime.datetime.now(),'tmp':temperature, 'hum':humidity, 'atm':press}
+        msg = {'time':srt(datetime.datetime.now()),'tmp':temperature, 'hum':humidity, 'atm':press}
         payload = json.dumps(msg)
         publish.single(TOPIC, payload, qos=0, hostname=BROKER, port=PORT)
         self.db.insert_db(temperature, humidity, press)
